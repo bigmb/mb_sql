07b9b39a0b827827870996f049fbf30d63e97194
bigmb <malav.b93@gmail.com>
2023-05-23 14:53:37 +0100

    update file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages,find_namespace_packages
+from mb_sql.version import version
+
+setup(
+    name="mb_sql",
+    version=version,
+    description="SQL functions package",
+    author=["Malav Bateriwala"],
+    packages=find_packages() + find_namespace_packages(include=["mb.*"]),
+    #packages=find_packages(),
+    scripts=[],
+    install_requires=[
+        "numpy",
+        "mb_pandas",
+        "mb_utils",
+        "sqlalchemy",
+        "pandas"],
+    python_requires='>=3.8',)
